1f00dd228ab9893f583d31fa39fa18e5107f2f88
sarpaslan <scharplover@gmail.com>
2024-03-24 10:20:26 +0300

    server

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { Player } from "./player.js";
 import { Server } from "socket.io";
 import http from "http";
 
@@ -8,88 +7,132 @@ const io = new Server(server, {
     origin: ["http://localhost:8080"],
   },
 });
-let players = [];
+let players = new Map();
 let lobbies = [];
+const countdown = 3;
 
 io.on("connection", (socket) => {
+  socket.on("looking-for-game", (language) => {
+    var player = players.get(socket.id);
+    player.language = language;
+    players.set(socket.id, player);
+    console.log("User looking for game with language : " + language);
+
+    let foundLobby = null;
+    if (lobbies != null || lobbies != undefined) {
+      lobbies.forEach((lobby) => {
+        if (lobby.language == language) {
+          if (lobby.players.length <= 8) {
+            foundLobby = lobby;
+          }
+        }
+      });
+    }
+    if (foundLobby === null) {
+      let newLobby = {
+        code: Math.floor(1000 + Math.random() * 9000).toString(),
+        players: [],
+        language: language,
+        state: "waiting",
+        currentPlayerId: -1,
+        time: 20,
+      };
+      lobbies.push(newLobby);
+      console.log(
+        "there is no avaible lobby for this user creating one",
+        newLobby.code
+      );
+      foundLobby = newLobby;
+    }
+    socket.emit("lobby-found", foundLobby.code);
+  });
+  socket.on("join-lobby", (code) => {
+    console.log("player wants to join lobby with code", code);
+    var player = players.get(socket.id);
+    const lobby = lobbies.find((lobby) => lobby.code === code);
+    lobby.players.push(player);
+    io.to(code).emit("player-joined-lobby", player);
+
+    socket.join(code);
+    socket.emit("lobby-joined", lobby);
+
+    if (lobby.players.length >= 2 && lobby.state === "waiting") {
+      lobby.state = "countdown";
+      io.to(code).emit("start-countdown", countdown);
+      setTimeout(() => {
+        io.to(code).emit("start-game");
+        const randomPlayer =
+          lobby.players[Math.floor(Math.random() * lobby.players.length)];
+        lobby.currentPlayerId = randomPlayer.id;
+        lobby.state = "game";
+        io.to(code).emit("turn-changed", lobby.currentPlayerId);
+      }, countdown * 1000);
+    }
+  });
+
   socket.on("login", (name) => {
     if (name.length < 3 || name.length > 12) {
       return;
     }
-    const player = new Player(name);
-    player.id = socket.id;
-    players.push(player);
+    var player = {
+      id: socket.id,
+      name: name,
+      lobby: null,
+      language: "en",
+      heart: 2,
+    };
+    players.set(socket.id, player);
     socket.emit("logged-in");
-
-    socket.broadcast.emit("player-joined", player);
   });
 
   socket.on("disconnect", () => {
-    players = players.filter((player) => player.id !== socket.id);
-    socket.broadcast.emit("player-disconnected", {
-      playerId: socket.id,
-    });
-  });
-
-  socket.on("create-lobby", (playerName) => {
-    const lobby = {
-      code: Math.floor(1000 + Math.random() * 9000).toString(),
-      players: [{ id: socket.id, isHost: true, name: playerName }],
-    };
-    lobbies.push(lobby);
-    socket.emit("lobby-created", lobby);
-  });
-  socket.on("join-lobby", (request) => {
-    const lobby = lobbies.find((lobby) => lobby.code === request.code);
-    console.log(lobbies);
-    console.log("requested-join-lobby", request.code, "lobby", lobby);
-    if (lobby) {
-      lobby.players.push({ id: socket.id, isHost: false, name: request.name });
-      socket.emit("lobby-joined", lobby);
-    } else {
-      socket.emit("lobby-not-found");
-      console.log("lobby not found");
-    }
-  });
-  socket.on("start-game", (lobbyCode) => {
-    const lobby = lobbies.find((lobby) => lobby.code === lobbyCode);
-
-    if (lobby) {
-      io.to(lobby.code).emit("game-started", lobby);
-    }
-  });
-  socket.on("lobbies", () => {
-    socket.emit("lobbies", lobbies);
-  });
-  socket.on("leave-lobby", () => {
-    //find the lobby the player is in
-    const lobby = lobbies.find((lobby) =>
-      lobby.players.find((player) => player.id === socket.id)
-    );
-    //remove player from lobby
-    lobby.players = lobby.players.filter((player) => player.id !== socket.id);
-    //switch the host if the host left
-    if (lobby.players.length > 0 && lobby.players[0].isHost === false) {
-      lobby.players[0].isHost = true;
-      console.log("changing host to", lobby.players[0].name);
-    }
-    //check if lobby has any players if its not remove the lobby
-    if (lobby.players.length === 0) {
-      console.log("removing lobby no one is in it anymore");
-      lobbies = lobbies.filter((l) => l.code !== lobby.code);
-
-      console.log("lobby count", lobbies.length);
-      return;
-    }
-
-    //send new host and disconnect info to all players in the lobby
-    console.log("sending player-left to lobby", lobby.code);
-    io.to(lobby.code).emit("player-left", {
-      playerId: socket.id,
-      newHost: lobby.players[0],
-    });
+    leaveLobby(socket);
   });
 });
+
+function leaveLobby(socket) {
+  const lobby = lobbies.find((lobby) =>
+    lobby.players.find((player) => player.id === socket.id)
+  );
+  console.log("leave lobby called");
+  if (!lobby) {
+    return;
+  }
+  socket.leave(lobby.code);
+
+  console.log("player left the socket code is", lobby.code);
+
+  lobby.players = lobby.players.filter((player) => player.id !== socket.id);
+
+  if (lobby.players.length === 0) {
+    lobbies = lobbies.filter((l) => l.code !== lobby.code);
+    console.log("lobby count", lobbies.length);
+    return;
+  }
+
+  io.to(lobby.code).emit("player-left-lobby", {
+    id: socket.id,
+  });
+
+  if (lobby.state === "game") {
+    if (lobby.players.length === 1) {
+      console.log("only one player left game over", lobby.players[0].id);
+      lobby.state = "ending";
+      lobby.currentPlayerId = -1;
+      io.to(lobby.code).emit("game-over", lobby.players[0].id);
+      setTimeout(() => {
+        lobby.state = "waiting";
+      }, 3000);
+    }
+    if (lobby.currentPlayerId === socket.id) {
+      const nextPlayer = lobby.players.find(
+        (player) => player.id !== socket.id
+      );
+      lobby.currentPlayerId = nextPlayer.id;
+      io.to(lobby.code).emit("turn-changed", lobby.currentPlayerId);
+    }
+  }
+}
 
 const PORT = 3000;
 server.listen(PORT, () => {
